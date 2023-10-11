986e49da9f09b9cd9cc8c05b63df78acb5b1f4b0
Vương Hoàng Hiếu <hieuvh@crownx.masangroup.com>
2023-10-11 20:01:50 +0700

    Add project files.

---
+++ Run4Prize/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Run4Prize.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
